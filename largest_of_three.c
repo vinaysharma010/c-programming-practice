006b53be04b822d1c85f3627a3c14b9854868728
Vinay sharma <vinaysvd454@gmail.com>
2026-01-29 15:41:24 +0530

    Implement largest of three numbers in C
    Fix variable declaration and input/output issues.

---
+++ largest_of_three.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+  itn a , b , c ;
+printf("enter the number : ");
+scanf("%d %d %d", & &b &c);
+if(a>=b && a>=c )
+  printf("largest number is %d\n",a);
+else if (b>=a && b>=c)
+  printf("largest number is %d\n",b);
+else 
+  printf(largest number is %d\n,c);
+return 0;
+}
+  

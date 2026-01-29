59754f7ba62a346839091f7d43eb390bdc65ae91
Vinay sharma <vinaysvd454@gmail.com>
2026-01-29 15:45:09 +0530

    Correct variable type and improve input prompts

--- largest_of_three.c
+++ largest_of_three.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main() {
-  itn a , b , c ;
-printf("enter the number : ");
-scanf("%d %d %d", & &b &c);
+  int a , b , c ;
+printf("enter three numbers : ");
+scanf("%d %d %d", &a &b &c);
 if(a>=b && a>=c )
   printf("largest number is %d\n",a);
 else if (b>=a && b>=c)
   printf("largest number is %d\n",b);
 else 
-  printf(largest number is %d\n,c);
+  printf("largest number is %d\n",c);
 return 0;
 }
   
